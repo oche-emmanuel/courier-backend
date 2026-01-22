734fc530c22b4b2bbd75ff06a1a8a7be9556d129
pabbie-malay <ahmcoker20@gmail.com>
2026-01-22 07:49:13 -0500

    Fix: Update CORS origin to include new frontend URL

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: ['https://turbo-courier.onrender.com', 'http://localhost:5173'],
+  origin: [
+    'https://turbo-courier.onrender.com',
+    'https://courier-frontend-ms06.onrender.com',
+    'http://localhost:5173'
+  ],
   credentials: true
 }));
 app.use(express.json());
